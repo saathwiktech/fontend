964af05e1767bac554f9988df52d271ace276950
SATHWIK <sathwikacharya61@gmail.com>
2025-05-07 20:13:25 +0530

    g

--- src/pages/dashboard/Profile.jsx
+++ src/pages/dashboard/Profile.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Profile = () => (
   <div>
-    <h2>Your Profile</h2>
+    <h2>Your profile</h2>
     <p>View and update your profile information here.</p>
   </div>
 );
